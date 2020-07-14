b577ce62458c930df3447240d28edac7725768c3
milyasbpa <basilyas.2015@gmail.com>
2020-07-14 11:40:57 +0700

    Updates

---
+++ precache-manifest.5e38b18cf3a1c1cfd5bcec072a2cfc6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d0b7513b6e076d1b52e1e9f4c657a1",
+    "url": "/cicd/index.html"
+  },
+  {
+    "revision": "9c491555d9f2abebaf22",
+    "url": "/cicd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be9f9fc0ea297e165744",
+    "url": "/cicd/static/js/2.b50bd03c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cicd/static/js/2.b50bd03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c491555d9f2abebaf22",
+    "url": "/cicd/static/js/main.0f921a4a.chunk.js"
+  },
+  {
+    "revision": "5a6211632d820a900cac",
+    "url": "/cicd/static/js/runtime-main.43dc13a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cicd/static/media/logo.5d5d9eef.svg"
+  }
+]);
